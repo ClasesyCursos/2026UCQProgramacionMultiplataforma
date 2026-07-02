c999a37c7edd0fbd2a4201a953a407fe2c891a51
CesarMontiel <cmontielmoctezuma@gmail.com>
2026-07-01 18:33:27 -0600

    Refactor JavaScript code for imperative and declarative examples; add new HTML files for React implementation.

--- Ejercicio1/programacion_declarativa.js
+++ Ejercicio1/programacion_declarativa.js
@@ -1,9 +1,8 @@
-const numeros = [1, 2, 3, 4, 5, 6];
-
 const evalPar = n => n % 2 === 0;
 
 console.log("Resultado:", evalPar(54)); 
 
-const resultado = numeros.filter(n => n % 2 === 0).map(n => n * 2);
+const resultadodeclarativo = 
+    numeros.filter(n => n % 2 === 0).map(n => n * 2);
 
-console.log(resultado); // [4, 8, 12]
+console.log(resultadodeclarativo); // [4, 8, 12]
--- Ejercicio1/programacion_imperativa.js
+++ Ejercicio1/programacion_imperativa.js
@@ -1,10 +1,10 @@
 const numeros = [1, 2, 3, 4, 5, 6];
-const resultado = [];
+const resultadoimperativo = [];
 
 for (let i = 0; i < numeros.length; i++) {
   if (numeros[i] % 2 === 0) {
-    resultado.push(numeros[i] * 2);
+    resultadoimperativo.push(numeros[i] * 2);
   }
 }
 
-console.log(resultado); // [4, 8, 12]
+console.log(resultadoimperativo); // [4, 8, 12]
